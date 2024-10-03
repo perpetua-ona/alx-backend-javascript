9b102ee4e9d16f8a3c061635a18993a74cb17a56
perpetua-ona <onaperpetua@gmail.com>
2024-10-03 01:45:01 +0000

    Alx ES6 Basic, Configuration and files

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,66 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"12-createReportObject.js" [New File]                                                                                                                                                           0,0-1         All
+
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,2 @@
+ALX Project, ES6 Basic
+======================
